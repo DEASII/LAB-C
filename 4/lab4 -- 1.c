9a4df8d9c06afce046f7a315d8c4154c9e8db1a9
DEASII <121237034+DEASII@users.noreply.github.com>
2022-12-22 21:20:05 +0700

    Create lab4 -- 1.c

---
+++ 4/lab4 -- 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+//int num = 7;
+char num_str[6];
+int num;
+fgets(num_str, 6, stdin);
+num = atoi(num_str);
+printf("Binary number of %d is ",num);
+for (int i = 3 ; i >= 0; i--){
+    printf("%d",(num>>i)%2);
+}
+puts(".");
+}
